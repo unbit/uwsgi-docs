8afde0090460d7a91c7e2c1af699aec01e9a4cdc
Unbit <info@unbit.it>
2014-06-08 06:56:23 +0200

    fixed reference

--- optdefs.pl
+++ optdefs.pl
@@ -1,5 +1,5 @@
 $OPTIONS = {
-	'emperor' => {'ref' => 'Emperor', 'doc' => "The Emperor is a special uWSGI instance aimed at governing other uWSGI instances (named: vassals). By default it is configured to monitor a directory containing valid uWSGI config files, whenever a fiel is created a new instance is spawned, when the file is touched the instance is reloaded, when the file is removed the instance is destroyed. It can be extended to support more paradigms"},
+	'emperor' => {'ref' => 'Emperor', 'doc' => "The Emperor is a special uWSGI instance aimed at governing other uWSGI instances (named: vassals). By default it is configured to monitor a directory containing valid uWSGI config files, whenever a file is created a new instance is spawned, when the file is touched the instance is reloaded, when the file is removed the instance is destroyed. It can be extended to support more paradigms"},
 	'thunder-lock' => {'ref' => 'articles/SerializingAccept'},
 	'declare-option' => {'ref' => 'CustomOptions'},
 	'fastrouter' => {'ref' => 'Fastrouter'},
