f09505571e983cfd455f6d19adf2705dd09c5684
Ekevoo <ekevoo@users.noreply.github.com>
2019-12-12 10:20:53 -0500

    Update optdefs.pl
    I originally thought that option was a boolean. I have no idea if that's correct or not, I just found it here http://lists.unbit.it/pipermail/uwsgi/2011-March/001594.html

--- optdefs.pl
+++ optdefs.pl
@@ -9,4 +9,5 @@ $OPTIONS = {
 	'stats-no-cores' => {'ref' => 'Metrics', 'doc' => "Do not expose the information about cores in the stats server."},
 	'stats-no-metrics' => {'ref' => 'Metrics', 'doc' => "Do not expose the metrics at all in the stats server."},
 	'buffer-size' => {'doc' => "Set the max size of a request (request-body excluded), this generally maps to the size of request headers. By default it is 4k. If you receive a bigger request (for example with big cookies or query string) you may need to increase it. It is a security measure too, so adapt to your app needs instead of maxing it out."},
+	'cpu-affinity' => {'doc' => "The number of CPUs to dedicate per worker."},
 };
